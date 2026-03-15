a0ff65c22822ddf596ac0d34375732f13993a9ce
volturra <86893133+volturra@users.noreply.github.com>
2026-03-14 23:11:17 -0400

    Update enable_nvidia_595_sleep.sh

--- enable_nvidia_595_sleep.sh
+++ enable_nvidia_595_sleep.sh
@@ -1,6 +1,19 @@
 # This is a set of instructions for Fedora 43 attempting to run nVidia 595 drivers from Negativo17
 # I ran into a problem where S3 sleep worked fine on 590, but the switch to 595 broke that ability.
 
+# At least part of the issue was the switch from various nvidia daemons that ran under
+# unbound permissions to using the native kernel daemon, which ultimately delegates operations to 
+# systemd-sleep, which in turn does not have the correct permissions to write out the files.
+
+# Further, the nVidia kernel checks if there is sufficient space at the target location
+# /tmp by default is 1/2 RAM. That's 16GB in my case. It's also not empty. The desired amount of space
+# to dump out VRAM is slightly larget than the VRAM. For a 5070Ti, that means the driver is looking for 
+# >16GB available, even if it intends to dump out only a gig or two.
+# I made a separate mem mount for it that is larger than the desired amount.
+# Potential issue, of course, is if I try to sleep with a bunch of VMs up and VRAM loaded, I will OOM. 
+# Don't do that. Alt, of course, write it to /var/tmp or some other disk, but I'm trading speed
+# and SSD endurance here for ensurance of no issues.
+
 # Use these to check last boot journalctl
 # journalctl -b -1 -e 
 # journalctl -b -1 -k | grep -iE '(suspend|sleep|nvidia|acpi|freeze|oom|error)' | tail -n 50 
